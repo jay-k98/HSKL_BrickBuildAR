20a5dd478021df519d8a89539adc7fbdf950a7f6
AaSa98 <aa.sntmr@gmail.com>
2021-12-28 14:53:49 +0100

    beide inputsysteme, arplane + platzieren des objektes

--- Assets/Scripts/ARTapToPlace.cs
+++ Assets/Scripts/ARTapToPlace.cs
@@ -6,22 +6,28 @@ using UnityEngine.XR.ARSubsystems;
 
 [RequireComponent(typeof(ARRaycastManager))]
 
-public class TapToPlace : MonoBehaviour
+public class ARTapToPlace : MonoBehaviour
 {
-    public GameObject objectToPlace;
+    //Objekt, das gespawnt werden soll
+    public GameObject gameObjectToInstantiate;
+
     private GameObject spawnedObject;
-    private ARRaycastManager _arRaycastManager;
+
+    private ARRaycastManager _arRayCastManager;
+
     private Vector2 touchPosition;
+
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
 
     private void Awake()
     {
-        _arRaycastManager = GetComponent<ARRaycastManager>();
+        _arRayCastManager = GetComponent<ARRaycastManager>();
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
     {
-        if (Input.touchCount > 0)
+        if(Input.touchCount > 0)
         {
             touchPosition = Input.GetTouch(0).position;
             return true;
@@ -36,14 +42,15 @@ public class TapToPlace : MonoBehaviour
     {
         if (!TryGetTouchPosition(out Vector2 touchPosition))
             return;
-        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        if(_arRayCastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
         {
             var hitPose = hits[0].pose;
-            Debug.Log(hits[0]);
+
             if(spawnedObject == null)
             {
-                spawnedObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
             }
+
             else
             {
                 spawnedObject.transform.position = hitPose.position;
